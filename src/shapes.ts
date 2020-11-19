dfc5305d637802870fafc2790c4b580af2c47631
James T Allan <jtallan@ymail.com>
2020-11-18 23:30:37 -0500

    types

--- src/shapes.ts
+++ src/shapes.ts
@@ -15,6 +15,46 @@ interface GetBoxscoreForWeekRequest {
   scoringPeriodId: number;
 }
 
+enum PlayerAvailabilityStatuses {
+  FreeAgent = "FREEAGENT",
+  OnTeam = "ONTEAM",
+  Waivers = "WAIVERS",
+}
+
+//---------------------------------------------------
+interface Player {
+  id: number;
+  firstName: string;
+  lastName: string;
+  fullName: string;
+  jerseyNumber: number;
+  proTeam: string;
+  proTeamAbbreviation: string;
+  defaultPosition: string;
+  eligiblePositions: Array<string>;
+  averageDraftPosition: number;
+  averageAuctionValue: number;
+  percentChange: number;
+  percentStarted: number;
+  percentOwned: number;
+  acquiredDate: Date;
+  availabilityStatus: PlayerAvailabilityStatuses;
+  isDroppable: boolean;
+  isInjured: boolean;
+  injuryStatus: INJURY_STATUSES;
+}
+
+interface getFreeAgents {
+  seasonId: number;
+  scoringPeriodId: number;
+}
+
+interface FreeAgentPlayer {
+  player: Player;
+  rawStats: PlayerStats;
+  projectRawStats: PlayerStats;
+}
+
 export default interface ESPNClient {
   getBoxscoreForWeek(req: GetBoxscoreForWeekRequest): Promise<BoxScore[]>;
 }
